e83c6c40c1daf2a600897d75c0c51bcec1034ced
vicentegarinpe <168034514+vicentegarinpe@users.noreply.github.com>
2024-10-24 14:17:00 -0300

    class healer

---
+++ core/sprites/healer.py
@@ -0,0 +1,9 @@
+class Healer():
+    def __init__(self,health,dmg,speed): #healt =vida dmg=daño speed= velocidad
+        self.healt=health
+        self.dmg=dmg
+        self.speed=speed
+    def curar(): # metodo de la personaje curandera la cual curara su vida
+        pass
+    def ataque():  # metodo de ataque
+        pass
