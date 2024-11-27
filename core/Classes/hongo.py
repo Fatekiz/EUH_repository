da8f1cd58520001cd1f32fb345ed36009826db7e
Fatekiz <benjamin.concha.m10@gmail.com>
2024-11-27 14:25:34 -0300

    meros cambios tontos

--- core/Classes/hongo.py
+++ core/Classes/hongo.py
@@ -1,10 +1,9 @@
 from core.Classes.npc import NPC
-import pygame
 
 class Hongo(NPC):
     def __init__(self, img, speed, position=(0, 0), health=30, dmg=10):
         super().__init__(img, health, dmg, speed, position)
 
     def update(self):
-        """Actualiza la posición del hongo (puede agregar comportamientos únicos si es necesario)."""
-        super().update()  # Llama al método `update` de la clase base
+        """(aquí se puede agregar comportamientos únicos si es necesario)."""
+        super().update()  # Llama al método 'update' de la clase base.
